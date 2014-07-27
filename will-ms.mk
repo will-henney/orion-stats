25cac488928c91557abffb5977ca3f52455b6a4d
William Henney <will@henney.org>
2014-07-27 01:15:53 -0500

    Make tarball for Jane and send it to her

--- will-ms.mk
+++ will-ms.mk
@@ -1,5 +1,14 @@
 
 MS=will-ms-changes
+WILLFIGS=sf-vs-3d-panels.pdf vca-thin-vs-3d-panels.pdf vca-thick-vs-3d-panels.pdf
+
+all: tar-file-for-jane open-pdf
+
+
+tar-file-for-jane: ms-resubmit-will.tar.gz
+
+ms-resubmit-will.tar.gz: ms-resubmit-will.tex will-replies.org $(WILLFIGS)
+	tar -zcf $@ $^
 
 open-pdf: $(MS).pdf
 	open $(MS).pdf
